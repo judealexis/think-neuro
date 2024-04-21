8f5c9e9150ab95bd5d553967b19c4cd4e1844c81
JudeAlexis111 <60825550+JudeAlexis111@users.noreply.github.com>
2024-04-21 16:43:06 -0700

    added compressed files; removed huge poster section

---
+++ dist/assets/internship-9hYXIoxK.js
@@ -0,0 +1,117 @@
+import{S as Y,i as V,s as Z,g as ee,h as _,j as f,b as v,l as ne,n as I,e as b,k as N,a as C,q as d,v as L,x as $,r as p,y as ae,z as se,p as O,o as x,H as re,R as oe,F as le,c as j,m as q,t as D,d as S,f as P}from"./Repeater-wxO3m-ib.js";import{T as ce}from"./TextButton-znhSqK0f.js";import{C as de}from"./Carousel-WbT7rJFY.js";function B(s,t,r){const a=s.slice();return a[10]=t[r],a}function z(s,t,r){const a=s.slice();return a[13]=t[r],a}function G(s,t,r){const a=s.slice();return a[16]=t[r],a}function U(s,t,r){const a=s.slice();return a[19]=t[r],a}function F(s,t,r){const a=s.slice();return a[16]=t[r],a}function K(s,t,r){const a=s.slice();return a[19]=t[r],a}function ue(s){let t,r=s[13].text+"",a;return{c(){t=f("div"),a=x(r),d(t,"id",s[13].label),d(t,"class","svelte-1qbi7g")},m(e,i){v(e,t,i),p(t,a)},p:I,d(e){e&&b(t)}}}function he(s){let t;function r(i,n){return i[13].label=="list"?me:ge}let e=r(s)(s);return{c(){e.c(),t=O()},m(i,n){e.m(i,n),v(i,t,n)},p(i,n){e.p(i,n)},d(i){i&&b(t),e.d(i)}}}function ge(s){let t,r,a=_(s[13].children),e=[];for(let i=0;i<a.length;i+=1)e[i]=H(G(s,a,i));return{c(){t=f("div");for(let i=0;i<e.length;i+=1)e[i].c();r=C(),d(t,"id",s[13].label),d(t,"class","svelte-1qbi7g")},m(i,n){v(i,t,n);for(let o=0;o<e.length;o+=1)e[o]&&e[o].m(t,null);p(t,r)},p(i,n){if(n&8){a=_(i[13].children);let o;for(o=0;o<a.length;o+=1){const l=G(i,a,o);e[o]?e[o].p(l,n):(e[o]=H(l),e[o].c(),e[o].m(t,r))}for(;o<e.length;o+=1)e[o].d(1);e.length=a.length}},d(i){i&&b(t),N(e,i)}}}function me(s){let t,r,a=_(s[13].children),e=[];for(let i=0;i<a.length;i+=1)e[i]=J(F(s,a,i));return{c(){t=f("ul");for(let i=0;i<e.length;i+=1)e[i].c();r=C(),d(t,"class","lineAdjustment svelte-1qbi7g")},m(i,n){v(i,t,n);for(let o=0;o<e.length;o+=1)e[o]&&e[o].m(t,null);p(t,r)},p(i,n){if(n&8){a=_(i[13].children);let o;for(o=0;o<a.length;o+=1){const l=F(i,a,o);e[o]?e[o].p(l,n):(e[o]=J(l),e[o].c(),e[o].m(t,r))}for(;o<e.length;o+=1)e[o].d(1);e.length=a.length}},d(i){i&&b(t),N(e,i)}}}function pe(s){let t,r=s[16].text+"",a;return{c(){t=f("div"),a=x(r),d(t,"id",s[16].label),d(t,"class","svelte-1qbi7g")},m(e,i){v(e,t,i),p(t,a)},p:I,d(e){e&&b(t)}}}function fe(s){let t,r=_(s[16].children),a=[];for(let e=0;e<r.length;e+=1)a[e]=M(U(s,r,e));return{c(){for(let e=0;e<a.length;e+=1)a[e].c();t=O()},m(e,i){for(let n=0;n<a.length;n+=1)a[n]&&a[n].m(e,i);v(e,t,i)},p(e,i){if(i&8){r=_(e[16].children);let n;for(n=0;n<r.length;n+=1){const o=U(e,r,n);a[n]?a[n].p(o,i):(a[n]=M(o),a[n].c(),a[n].m(t.parentNode,t))}for(;n<a.length;n+=1)a[n].d(1);a.length=r.length}},d(e){e&&b(t),N(a,e)}}}function M(s){let t,r=s[19].text+"",a;return{c(){t=f("span"),a=x(r),d(t,"id",s[19].label),d(t,"class","svelte-1qbi7g")},m(e,i){v(e,t,i),p(t,a)},p:I,d(e){e&&b(t)}}}function H(s){let t;function r(i,n){return i[16].children.length>0?fe:pe}let e=r(s)(s);return{c(){t=f("div"),e.c(),d(t,"class","lineAdjustment svelte-1qbi7g"),d(t,"id",s[16].label)},m(i,n){v(i,t,n),e.m(t,null)},p(i,n){e.p(i,n)},d(i){i&&b(t),e.d()}}}function W(s){let t,r=s[19].text+"",a;return{c(){t=f("span"),a=x(r),d(t,"id",s[19].label),d(t,"class","svelte-1qbi7g")},m(e,i){v(e,t,i),p(t,a)},p:I,d(e){e&&b(t)}}}function J(s){let t,r=_(s[16].children),a=[];for(let e=0;e<r.length;e+=1)a[e]=W(K(s,r,e));return{c(){t=f("li");for(let e=0;e<a.length;e+=1)a[e].c();d(t,"class","svelte-1qbi7g")},m(e,i){v(e,t,i);for(let n=0;n<a.length;n+=1)a[n]&&a[n].m(t,null)},p(e,i){if(i&8){r=_(e[16].children);let n;for(n=0;n<r.length;n+=1){const o=K(e,r,n);a[n]?a[n].p(o,i):(a[n]=W(o),a[n].c(),a[n].m(t,null))}for(;n<a.length;n+=1)a[n].d(1);a.length=r.length}},d(e){e&&b(t),N(a,e)}}}function Q(s){let t;function r(i,n){return i[13].children.length>0?he:ue}let e=r(s)(s);return{c(){e.c(),t=O()},m(i,n){e.m(i,n),v(i,t,n)},p(i,n){e.p(i,n)},d(i){i&&b(t),e.d(i)}}}function X(s){let t,r,a,e,i,n,o,l,w,y,R,k=_(s[10].text),c=[];for(let h=0;h<k.length;h+=1)c[h]=Q(z(s,k,h));return{c(){t=f("div"),r=f("div"),r.textContent=`${s[10].title}`,a=C(),e=f("div"),i=f("div"),n=f("img"),w=C(),y=f("div");for(let h=0;h<c.length;h+=1)c[h].c();R=C(),d(r,"id","header"),d(r,"class","svelte-1qbi7g"),d(n,"id","cardImage"),L(n.src,o=(s[2]>500?"assets/projects/":"assets/projects/stretched/")+s[10].image)||d(n,"src",o),d(n,"alt","Card"),d(n,"class","svelte-1qbi7g"),d(i,"id","imageWrapper"),d(i,"class","svelte-1qbi7g"),ee(()=>s[6].call(i)),d(y,"id","textDisc"),d(y,"class","inColumn svelte-1qbi7g"),$(y,"max-height",s[0]+"px"),d(e,"id","holder"),d(e,"class","inRow svelte-1qbi7g"),d(t,"id","card"),d(t,"class","inColumn svelte-1qbi7g")},m(h,m){v(h,t,m),p(t,r),p(t,a),p(t,e),p(e,i),p(i,n),l=ae(i,s[6].bind(i)),p(e,w),p(e,y);for(let g=0;g<c.length;g+=1)c[g]&&c[g].m(y,null);s[7](y),p(t,R)},p(h,m){if(m&4&&!L(n.src,o=(h[2]>500?"assets/projects/":"assets/projects/stretched/")+h[10].image)&&d(n,"src",o),m&8){k=_(h[10].text);let g;for(g=0;g<k.length;g+=1){const u=z(h,k,g);c[g]?c[g].p(u,m):(c[g]=Q(u),c[g].c(),c[g].m(y,null))}for(;g<c.length;g+=1)c[g].d(1);c.length=k.length}m&1&&$(y,"max-height",h[0]+"px")},d(h){h&&b(t),l(),N(c,h),s[7](null)}}}function ve(s){let t,r,a;ee(s[5]);let e=_(s[3]),i=[];for(let n=0;n<e.length;n+=1)i[n]=X(B(s,e,n));return{c(){t=f("main");for(let n=0;n<i.length;n+=1)i[n].c()},m(n,o){v(n,t,o);for(let l=0;l<i.length;l+=1)i[l]&&i[l].m(t,null);r||(a=ne(window,"resize",s[5]),r=!0)},p(n,[o]){if(o&15){e=_(n[3]);let l;for(l=0;l<e.length;l+=1){const w=B(n,e,l);i[l]?i[l].p(w,o):(i[l]=X(w),i[l].c(),i[l].m(t,null))}for(;l<i.length;l+=1)i[l].d(1);i.length=e.length}},i:I,o:I,d(n){n&&b(t),N(i,n),r=!1,a()}}}function te(s){s.sort((t,r)=>t.order-r.order),s.forEach(t=>{t.children&&t.children.length>0&&te(t.children)})}function be(s,t,r){let a,e,i,{prop:n}=t,o=[["λ","Λ","list"],["π","Π","point"],["ε","Ε","emphasis"],["τ","Τ","title"],["β","Β","body"],["ρ","Ρ","break"]];function l(c,h){let m=[],g=[];for(let u=0;u<o.length;u++){let T=o[u][0],E=o[u][1],ie=new RegExp(`(?<=${T})[^${T}${E}]*?(?=${E})`,"g"),A;for(;(A=ie.exec(c))!==null;)m.push({start:A.index+h,end:A.index+h+A[0].length,text:A[0],label:o[u][2],children:[]})}m.sort((u,T)=>u.start-T.start||T.end-u.end-(u.end-u.start));for(let u=0;u<m.length;u++){let T=!1;for(let E=u-1;E>=0;E--)if(m[u].start>=m[E].start&&m[u].end<=m[E].end){m[E].children.push(m[u]),T=!0;break}T||g.push(m[u])}return g.forEach(u=>{u.children.length>0&&(u.children=l(u.text,u.start))}),g}let w=[];for(let c=0;c<n.length;c++){let h=l(n[c].body,0);te(h),w.push({image:n[c].image,title:n[c].title,text:h})}function y(){r(2,a=window.innerWidth)}function R(){e=this.clientHeight,r(0,e)}function k(c){se[c?"unshift":"push"](()=>{i=c,r(1,i)})}return s.$$set=c=>{"prop"in c&&r(4,n=c.prop)},r(2,a=0),[e,i,a,w,n,y,R,k]}class _e extends Y{constructor(t){super(),V(this,t,be,ve,Z,{prop:4})}}function ye(s){let t,r,a,e,i,n,o;return r=new re({}),e=new oe({props:{children:[{title:"PROJECTS",orientation:"left",component:ce,prop:["Think Neuro offers research-based internships with topics encompassing health technology, health education, and health policy. Accepted interns are able to choose the rotation of their choice.",!0,"click","Apply","https://docs.google.com/forms/d/e/1FAIpQLScNgmqC-qtYnHcXUzsieLNrf2_2RT4Ly8Ic75GSOzS4xnDdmA/closedform"]},{title:"CURRENT OPPORTUNITIES",orientation:"left",component:_e,prop:s[0]},{title:"STUDENT TESTIMONIALS",orientation:"center",component:de,prop:[3,"assets/testimonials/internship/"]}]}}),n=new le({}),{c(){t=f("main"),j(r.$$.fragment),a=C(),j(e.$$.fragment),i=C(),j(n.$$.fragment)},m(l,w){v(l,t,w),q(r,t,null),p(t,a),q(e,t,null),p(t,i),q(n,t,null),o=!0},p:I,i(l){o||(D(r.$$.fragment,l),D(e.$$.fragment,l),D(n.$$.fragment,l),o=!0)},o(l){S(r.$$.fragment,l),S(e.$$.fragment,l),S(n.$$.fragment,l),o=!1},d(l){l&&b(t),P(r),P(e),P(n)}}}function we(s){return[[{image:"opioid.jpg",title:"Opioid Education Outreach in San Bernardino",body:`τBACKGROUNDΤ βThe ongoing opioid epidemic presents considerable challenges in communities
+      like San Bernardino, particularly impacting those with limited English proficiency, non-native
+      English speakers, and individuals for whom English is a second language. Addressing barriers to
+      accurate information access is vital to empower these populations, enhance education about opioids,
+      and ultimately reduce misuse while improving patient outcomes.Β ρ Ρ τDESCRIPTIONΤ βInterns will play a crucial role
+      in conducting community outreach, developing educational materials, and administering surveys to gather
+      baseline data, contributing to a comprehensive project aimed at raising awareness of opioid use and abuse
+      in San Bernardino.Β 
+      λ 
+      πεInnovative Educational Materials:ΕβThe interns will be actively involved in creating 
+      impactful educational materials, such as infographics, short papers, or videos, specifically designed to 
+      engage individuals struggling with opioid addiction.ΒΠ
+      πεCommunity Outreach and Surveys:ΕβThe intern cohort will play a pivotal role in conducting community 
+      outreach, distributing the educational materials, and administering initial surveys to gather baseline 
+      data on substance abuse within the targeted populations.ΒΠ 
+      πεProject Timeline and Follow-up:ΕβThe project will unfold in phases, starting with outreach to identify 
+      educational needs, followed by material development, and concluding with dispersal efforts. The interns 
+      will also engage in follow-up outreach to study participants, measuring quantitative improvements for 
+      statistical significance.ΒΠ 
+      Λ`.replace(/\s+/g," ").trim()},{image:"inter.jpg",title:"Digital Intervention Platform for Opioid Addiction",body:`τBACKGROUNDΤ βIn response to the pressing challenges of opioid addiction, particularly in unhoused 
+      and uninsured communities, there is a need for innovative, tech-enabled solutions to aid in the recovery 
+      process. The impact of addiction extends across diverse socioeconomic backgrounds, making it imperative to 
+      address these challenges with a focus on inclusivity. Through the development of a supportive platform, this 
+      initiative strives to provide continuous assistance, resources, and a sense of community for individuals in 
+      recovery.Β ρ Ρ τDESCRIPTIONΤ βInterns will be instrumental in developing a comprehensive digital intervention 
+      platform focused on aiding individuals in their recovery from opioid addiction. The platform will serve as a 
+      user-friendly, accessible resource offering various features, including: Β 
+      λ 
+      πεEducation Modules:ΕβInteractive modules providing information on the science of addiction, the impact on 
+      the brain, and strategies for recovery.ΒΠ
+      πεProgress Tracking:ΕβTools to track and celebrate milestones in recovery, encouraging ongoing engagement.ΒΠ 
+      πεResource Hub:ΕβAccess to local treatment centers, support services, and mental health resources.ΒΠ 
+      Λ
+      τINTERN ENGAGEMENTΤ
+      λ 
+      πεDesigning the platform:ΕβContributing to the user interface and experience design to ensure accessibility and engagement.ΒΠ
+      πεCreating content:ΕβDeveloping informative and engaging content for the education modules and resource hub.ΒΠ 
+      πεTesting usability:ΕβConducting thorough testing to ensure the platform is user-friendly and meets the diverse needs of its audienceΒΠ
+      πεDevising and implementing effective outreach strategies:Εβpromoting the platform within the affected communities, ensuring maximum 
+      impact and utilization.ΒΠ 
+      Λ
+      `.replace(/\s+/g," ").trim()},{image:"advo.jpg",title:"Policy Advocacy for Substance Abuse Treatment",body:`τBACKGROUNDΤ βRecognizing the entrenched systemic barriers hindering access to substance abuse treatment, 
+      there is an urgent need for policy changes that enhance both affordability and accessibility.Β ρ Ρ τDESCRIPTIONΤ 
+      βEmbark on a dynamic policy advocacy project dedicated to improving substance abuse treatment accessibility. 
+      Key tasks encompass:Β 
+      λ 
+      πεPolicy Analysis:ΕβThoroughly research and analyze existing state and federal policies pertaining to substance 
+      abuse treatment, identifying critical areas for enhancement.ΒΠ
+      πεStakeholder Engagement:ΕβEstablish collaborative partnerships by reaching out to advocacy groups, healthcare professionals, 
+      and policymakers, creating a robust coalition supporting enhanced accessibility.ΒΠ 
+      πεLegislative Recommendations:ΕβCraft tangible and actionable legislative recommendations aimed at addressing identified 
+      gaps within substance abuse treatment policy.ΒΠ 
+      πεCommunity Awareness Campaign:ΕβSpearhead the creation of impactful materials and events designed to raise awareness 
+      about the significance of policy changes, fostering community support for this crucial initiative.ΒΠ 
+      Λ
+      τINTERN ENGAGEMENTΤ
+      λ 
+      πεResearch:ΕβFostering a deep understanding of the policy landscape and identifying potential areas for improvement, ultimately 
+      equipped with the knowledge essential for effective advocacy.ΒΠ
+      πεCommunication:ΕβDeveloping and implementing effective communication strategies. This involves forging connections with advocacy 
+      groups, healthcare professionals, and policymakers to build a strong and supportive coalition. Interns play a pivotal role in 
+      amplifying the advocacy message.ΒΠ 
+      πεCommunity engagement:ΕβLeverage unique skills to create materials and organize events that resonate with diverse audiences. 
+      Interns become ambassadors, engaging communities and driving awareness about the importance of policy changes.ΒΠ
+      Λ
+      `.replace(/\s+/g," ").trim()},{image:"tele.jpg",title:"Telehealth Expansion for Addiction Treatment",body:`τBACKGROUNDΤ βThe Telehealth Expansion for Addiction Treatment project stems from a critical need to address 
+      pervasive barriers hindering access to addiction treatment, particularly in rural or underserved areas. The existing 
+      landscape reveals a stark reality where geographical remoteness and limited healthcare infrastructure converge to 
+      create significant hurdles for individuals seeking addiction treatment.Β ρ Ρ τDESCRIPTIONΤ 
+      βDevelop and implement a project to expand telehealth services for addiction treatment, including:Β 
+      λ 
+      πεNeeds Assessment:ΕβIdentify regions with limited access to addiction treatment services and assess their telehealth infrastructure.ΒΠ
+      πεPartnerships:ΕβCollaborate with healthcare providers, insurers, and telehealth platforms to establish and promote telehealth services 
+      for addiction treatment.ΒΠ 
+      πεUser-Friendly Resources:ΕβCreate educational materials to guide individuals through the process of accessing addiction treatment through 
+      telehealth.ΒΠ 
+      πεResearch, Outreach, and Resource Development:ΕβInvolved in multifaceted roles, encompassing thorough research, targeted outreach efforts, 
+      and the development of resources. Collective efforts aim to support individuals seeking addiction treatment through telehealth, 
+      addressing unique challenges in rural or underserved areas.ΒΠ 
+      Λ
+      `.replace(/\s+/g," ").trim()},{image:"lit.jpg",title:"Children’s Neurologic Literature",body:`τBACKGROUNDΤ βAddress barriers to accessing addiction treatment by expanding telehealth services, especially in rural or 
+      underserved areas.Β ρ Ρ τDESCRIPTIONΤ 
+      βConstruct and create empathetic and educational children’s books tailored towards those facing various neurodegenerative diseases 
+      including ADHD, dyslexia, and epilepsy. With a team of other interns, you will collaborate on various literary projects with the 
+      overarching goals of:Β 
+      λ 
+      πεEmpathy and Understanding:ΕβFoster empathy and understanding towards children dealing with neurodegenerative disease and their families.ΒΠ
+      πεAwareness and Education:ΕβRaise awareness about various neurological diseases and the specific needs of affected individuals while advocating 
+      for inclusivity and support within communities and healthcare systems.ΒΠ 
+      πεPositive Emotional Impact:ΕβBring joy, comfort, and moments of happiness to youth facing challenging circumstances through uplifting and 
+      inspiring stories.ΒΠ
+      Λ
+      βNote for Spring 2024, the Children’s Neurologic Literature Project is only accepting novel illustrations from the new cohort.Β
+      `.replace(/\s+/g," ").trim()},{image:"lang.jpg",title:"Second Medical Language Exchange",body:`τBACKGROUNDΤ βWithin the landscape of healthcare, a significant impediment to equitable access and quality of care is the 
+      documented dearth of educational materials available in languages diverse from the predominant ones. This discrepancy serves as a 
+      formidable barrier, perpetuating disparities across chronic health management, treatments, and overall healthcare outcomes. The 
+      challenges are profound: language disparities, cultural sensitivity, impact on health outcomes, and age and cultural considerations.Β 
+      ρ Ρ τDESCRIPTIONΤ 
+      βThis initiative aims to bridge the existing gap between medical professionals and Limited English Proficiency (LEP) populations by 
+      creating culturally sensitive and linguistically tailored educational materials. The project's objectives extend beyond merely generating 
+      new materials; it aspires to enhance existing resources and investigate how these improvements can positively impact healthcare outcomes 
+      for LEP populations. The key objectives include:Β 
+      λ 
+      πεMaterial Development:ΕβCraft educational materials that resonate with diverse linguistic and cultural backgrounds. These materials 
+      will not only address current gaps but will also be designed to improve understanding and engagement between medical professionals 
+      and LEP populations.ΒΠ
+      πεOutcome Evaluation:ΕβConduct a comprehensive study to assess how the improved and new educational materials contribute to enhanced 
+      healthcare outcomes for LEP populations. This evaluation will provide valuable insights into the efficacy of culturally sensitive 
+      resources.ΒΠ 
+      πεCultural and Age Considerations:ΕβExplore the nuanced dynamics of culture and age in educational material effectiveness. This 
+      involves understanding how different cultural contexts and age groups respond to healthcare information, enabling the establishment 
+      of general guidelines for creating universally effective educational materials.ΒΠ
+      πεGuideline Establishment:ΕβBased on the study's findings, establish general guidelines for creating educational materials that are 
+      optimally effective for both medical professionals and LEP populations. These guidelines will serve as a resource for future initiatives 
+      aimed at promoting healthcare equity and access.ΒΠ
+      Λ
+      `.replace(/\s+/g," ").trim()}]]}class ke extends Y{constructor(t){super(),V(this,t,we,ye,Z,{})}}new ke({target:document.body,props:{name:"world"}});
--- src/Research.svelte
+++ src/Research.svelte
@@ -38,105 +38,142 @@
   noAlternate={false}
   children={[
     {
-      title: "GALLERY (2023)",
+      title: "GALLERY (2024)",
       orientation: "left",
-      component: JuxtaposeGallery,
+      component: TightGallery,
       prop: [
-        [
-          {
-            component: Image,
-            args: "assets/posters/2023/0.png",
-          },
-          {
-            component: Video,
-            args: "https://www.youtube.com/embed/oU8GnZgzF1I",
-          },
-        ],
-        [
-          {
-            component: Image,
-            args: "assets/posters/2023/1.png",
-          },
-          {
-            component: Video,
-            args: "https://www.youtube.com/embed/LTNZb70U7UE",
-          },
-        ],
-        [
-          {
-            component: Image,
-            args: "assets/posters/2023/2.png",
-          },
-          {
-            component: Video,
-            args: "https://www.youtube.com/embed/w_LOSchQXiI",
-          },
-        ],
-        [
-          {
-            component: Image,
-            args: "assets/posters/2023/3.png",
-          },
-          {
-            component: Video,
-            args: "https://www.youtube.com/embed/q4SLNHTE4h0",
-          },
-        ],
-        [
-          {
-            component: Image,
-            args: "assets/posters/2023/4.png",
-          },
-          {
-            component: Video,
-            args: "https://www.youtube.com/embed/bN03T3wWOJ0",
-          },
-        ],
-        [
-          {
-            component: Image,
-            args: "assets/posters/2023/5.png",
-          },
-          {
-            component: Video,
-            args: "https://www.youtube.com/embed/wQ0znzBUcD8",
-          },
-        ],
-        [
-          {
-            component: Image,
-            args: "assets/posters/2023/6.png",
-          },
-          {
-            component: Video,
-            args: "https://www.youtube.com/embed/eYLekIrFUTM",
-          },
-        ],
-        [
-          {
-            component: Image,
-            args: "assets/posters/2023/7.png",
-          },
-          {
-            component: Video,
-            args: "https://www.youtube.com/embed/OZ0nAbft6kY",
-          },
-        ],
-        [
-          {
-            component: Image,
-            args: "assets/posters/2023/8.png",
-          },
-          {
-            component: Video,
-            args: "https://www.youtube.com/embed/bsEkXE-GI4E",
-          },
-        ],
+        {
+          component: Image,
+          args: "assets/posters/2024/0.png",
+          three: false,
+        },
+        {
+          component: Image,
+          args: "assets/posters/2024/1.png",
+        },
+        {
+          component: Image,
+          args: "assets/posters/2024/2.png",
+        },
+        {
+          component: Image,
+          args: "assets/posters/2024/3.png",
+        },
+        {
+          component: Image,
+          args: "assets/posters/2024/4.png",
+        },
+        {
+          component: Image,
+          args: "assets/posters/2024/5.png",
+        },
+        {
+          component: Image,
+          args: "assets/posters/2024/6.png",
+        },
       ],
     },
   ]}
 />
 <Footer />
+
+<!-- {
+  title: "GALLERY (2023)",
+  orientation: "left",
+  component: JuxtaposeGallery,
+  prop: [
+    [
+      {
+        component: Image,
+        args: "assets/posters/2023/0.png",
+      },
+      {
+        component: Video,
+        args: "https://www.youtube.com/embed/oU8GnZgzF1I",
+      },
+    ],
+    [
+      {
+        component: Image,
+        args: "assets/posters/2023/1.png",
+      },
+      {
+        component: Video,
+        args: "https://www.youtube.com/embed/LTNZb70U7UE",
+      },
+    ],
+    [
+      {
+        component: Image,
+        args: "assets/posters/2023/2.png",
+      },
+      {
+        component: Video,
+        args: "https://www.youtube.com/embed/w_LOSchQXiI",
+      },
+    ],
+    [
+      {
+        component: Image,
+        args: "assets/posters/2023/3.png",
+      },
+      {
+        component: Video,
+        args: "https://www.youtube.com/embed/q4SLNHTE4h0",
+      },
+    ],
+    [
+      {
+        component: Image,
+        args: "assets/posters/2023/4.png",
+      },
+      {
+        component: Video,
+        args: "https://www.youtube.com/embed/bN03T3wWOJ0",
+      },
+    ],
+    [
+      {
+        component: Image,
+        args: "assets/posters/2023/5.png",
+      },
+      {
+        component: Video,
+        args: "https://www.youtube.com/embed/wQ0znzBUcD8",
+      },
+    ],
+    [
+      {
+        component: Image,
+        args: "assets/posters/2023/6.png",
+      },
+      {
+        component: Video,
+        args: "https://www.youtube.com/embed/eYLekIrFUTM",
+      },
+    ],
+    [
+      {
+        component: Image,
+        args: "assets/posters/2023/7.png",
+      },
+      {
+        component: Video,
+        args: "https://www.youtube.com/embed/OZ0nAbft6kY",
+      },
+    ],
+    [
+      {
+        component: Image,
+        args: "assets/posters/2023/8.png",
+      },
+      {
+        component: Video,
+        args: "https://www.youtube.com/embed/bsEkXE-GI4E",
+      },
+    ],
+  ],
+}, -->
 
 <style>
 </style>
