85521212d999fb4df09cbb321eb388d1450b88d8
JudeAlexis111 <60825550+JudeAlexis111@users.noreply.github.com>
2024-04-21 15:18:07 -0700

    hopefull pages fix

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,18 @@ export default defineConfig({
     plugins: [svelte()],
     base: '/',
     build: {
-        assetsInclude: ['**/*.ttf']
+        rollupOptions: {
+          input: {
+            main: 'index.html',
+            about: 'about.html',
+            faq: 'faq.html',
+            internship: 'internship.html',
+            newsletter: 'newsletter.html',
+            research: 'research.html',
+            ty: 'ty.html',
+            volunteer: 'volunteer.html',
+            // Add other HTML files as needed
+          }
+        }
       }
 })
